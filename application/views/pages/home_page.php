311a144b4df8f1b822a72e6ec462c63ada410f50
Aditya Kuppa <adityakuppa27@gmail.com>
2018-06-03 08:13:15 +0530

    Update home_page.php

--- application/views/pages/home_page.php
+++ application/views/pages/home_page.php
@@ -47,28 +47,28 @@ header, footer {
   <h3 align='center'>Click on the below images to get details of the respective NGO's</h3>
   <div class="row"> -->
 <div class="container text-center" style="padding-bottom: 20px">    
-  <h3>Hello Everyone</h3>
-  <h3 align='center'>Click on the below images to get details of the respective NGO's</h3>
+  <h3 style="font-style: oblique;">The NGOs we trust to serve you.</h3>
+  <h3 align='center' style="font-style: oblique;">Get to know them more !!</h3>
   <div class="row">
     <div class="col-sm-4" >
  <fieldset>
         <legend><a href="https://www.google.com/"><img src="
 <?php echo base_url()?>/assets/images/speak.jpg" style="width:100%;padding-top:35px;" alt="ngo1"></a></legend>
-  This NGO deals with the speaking of people. Hello people, Manish is a nice boy . 
+  This NGO helps preserve the voice of the common public. 
  </fieldset>
     </div>
     <div class="col-sm-4"> 
  <fieldset>
       <legend><a href="https://www.google.com/"><img src="
 <?php echo base_url()?>/assets/images/splash.png" alt="Image" style="padding-top:18px;" width="70%" height="50%" ></a></legend>
- Save water save manish. Mnish is a nice boy.
+ This organisation works towards spreading awareness to save water.
  </fieldset>    
 </div>
     <div class="col-sm-4"> 
  <fieldset>
       <legend><a href="https://www.google.com/"><img src="
 <?php echo base_url()?>/assets/images/mama.png" alt="Image" style="padding-top:23px;" ></a>  </legend>
- Manish mother is proud of him.
+ Mama's children provides all basic necessities free of cost to the malnutritioned infants.
  </fieldset>  
     </div>
   </div>
